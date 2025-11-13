cf86bc4a6dcf691f70d0d927615b846e4a37f647
amrobasyouni <amrobasyoui6@gmail.com>
2025-11-13 21:50:51 +0200

    Added security to endpoints through spring starter security

---
+++ src/main/java/in/amrobasyouni/CashPilot/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package in.amrobasyouni.CashPilot.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->auth.requestMatchers("/register","/health","activate","status","login").permitAll().anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+           return httpSecurity.build();
+    }
+
+
+}
